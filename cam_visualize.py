2ecf7e2f64e039f7195f0b2af27f0dcbb66577df
yzouag <3310189877@qq.com>
2021-09-17 11:26:30 +0800

    modify cam_visualize

--- cam_visualize.py
+++ cam_visualize.py
@@ -1,13 +1,21 @@
 import numpy as np
 import cv2
+import os
 
-cam = np.load('cam_val/01.npy', allow_pickle=True).item()
-im = cv2.imread('Dataset/2.validation/img/01.png')
+img_path = 'Dataset/2.validation/img'
+image_names = os.listdir(img_path)
+cam_path = 'out_cam'
+npy_names = os.listdir(cam_path)
 
-heatmap = (cam[1] * 255).astype(np.uint8)
-# heatmap = np.expand_dims(heatmap,axis=2)
-heatmap_img = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+for i in range(len(image_names)):
+    cam = np.load(os.path.join(cam_path, npy_names[i]), allow_pickle=True)
+    im = cv2.imread(os.path.join(img_path, image_names[i]))
 
-fin = cv2.addWeighted(heatmap_img, 0.7, im, 0.3, 0)
-cv2.imshow('result.png',fin)
-cv2.waitKey(0)
+    heatmap = (cam[0] * 255).astype(np.uint8)
+    heatmap = np.expand_dims(heatmap,axis=2)
+    heatmap_img = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+    fin = cv2.addWeighted(heatmap_img, 0.7, im, 0.3, 0)
+    cv2.imshow('result.png',fin)
+    cv2.waitKey(0)
+    break
