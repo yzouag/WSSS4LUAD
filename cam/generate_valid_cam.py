44aaf6be8fe09e3e2b81f9628af636d164ebad27
yzouag <3310189877@qq.com>
2021-09-23 23:46:51 +0800

    small modify for path issues

--- cam/generate_valid_cam.py
+++ cam/generate_valid_cam.py
@@ -1,30 +1,32 @@
+import shutil
+from PIL import Image
+import argparse
+from tqdm import tqdm
+from torchvision import transforms
+import torch.nn.functional as F
+import numpy as np
+from torch.utils.data import DataLoader
+import dataset.dataset as dataset
+import network
+import torch
 from math import inf
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='2'
-import torch
-import network
-import dataset.dataset as dataset
-from torch.utils.data import DataLoader
-import numpy as np
-import torch.nn.functional as F
-from torchvision import transforms
-from tqdm import tqdm, trange
-import matplotlib.pyplot as plt
-import argparse
-from PIL import Image
-import shutil
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", action='store_true', help='whether it is to generate for validation set')
+parser.add_argument("-v", action='store_true',
+                    help='whether it is to generate for validation set')
 parser.add_argument("-side", default=96, type=int, required=False)
 parser.add_argument("-stride", default=32, type=int, required=False)
-parser.add_argument("-m", default="model_last", type=str, required=False, help="model name")
+parser.add_argument("-m", default="model_last", type=str,
+                    required=True, help="model name")
 args = parser.parse_args()
 
 for_validation = args.v
 side_length = args.side
 stride = args.stride
 model_name = args.m
+
 if for_validation:
     out_cam = "./validation_out_cam"
     dataset_path = "./Dataset/2.validation/img"
@@ -41,7 +43,7 @@ else:
 net = network.ResNetCAM()
 path = "./modelstates/" + model_name + ".pth"
 pretrained = torch.load(path)['model']
-pretrained = {k[7:] : v for k, v in pretrained.items()}
+pretrained = {k[7:]: v for k, v in pretrained.items()}
 pretrained['fc1.weight'] = pretrained['fc1.weight'].unsqueeze(-1).unsqueeze(-1).to(torch.float64)
 pretrained['fc2.weight'] = pretrained['fc2.weight'].unsqueeze(-1).unsqueeze(-1).to(torch.float64)
 
@@ -51,29 +53,27 @@ net.cuda()
 net.eval()
 
 onlineDataset = dataset.OnlineDataset(dataset_path, transform=transforms.Compose([
-    transforms.Resize((224,224)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
-    patch_size = side_length,
-    stride = stride)
+    patch_size=side_length,
+    stride=stride)
 
 print("Dataset", len(onlineDataset))
 onlineDataloader = DataLoader(onlineDataset, batch_size=1, drop_last=False)
 
 for im_path, im_list, position_list in tqdm(onlineDataloader):
     orig_img = np.asarray(Image.open(im_path[0]))
-
     def tocamlist(im):
-        
         im = im.cuda()
         cam_scores = net(im)
         # expected shape is batch_size * channel * h * w
-        cam_scores = F.interpolate(cam_scores, (side_length, side_length), mode='bilinear', align_corners=False)[0].detach().cpu().numpy()
+        cam_scores = F.interpolate(cam_scores, (side_length, side_length),
+                                   mode='bilinear', align_corners=False)[0].detach().cpu().numpy()
         return cam_scores
 
     # numofimgs, channels, length, lengths
     cam_list = list(map(tocamlist, im_list))
-    # print(len(cam_list))
 
     # merge crops
     sum_cam = np.zeros((3, orig_img.shape[0], orig_img.shape[1]))
@@ -81,8 +81,6 @@ for im_path, im_list, position_list in tqdm(onlineDataloader):
     for i in range(len(cam_list)):
         y, x = position_list[i][0], position_list[i][1]
         crop = cam_list[i]
-        # print("i is :",i)
-        # print(f"y: {y}, x: {x}")
         sum_cam[:, y:y+side_length, x:x+side_length] += crop
         sum_counter[:, y:y+side_length, x:x+side_length] += 1
     sum_counter[sum_counter < 1] = 1
@@ -97,7 +95,6 @@ for im_path, im_list, position_list in tqdm(onlineDataloader):
         if big_label[i] == 0:
             sum_cam[i, :, :] = -inf
     result_label = sum_cam.argmax(axis=0)
-
 
     if out_cam is not None:
         if not os.path.exists(out_cam):
