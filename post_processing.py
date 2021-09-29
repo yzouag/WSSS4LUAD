ca3817391be6088a8774cdf2337868c161a4c2eb
yzouag <3310189877@qq.com>
2021-09-29 20:02:06 +0800

    add big label

--- post_processing.py
+++ post_processing.py
@@ -1,8 +1,15 @@
+import json
+import os
 import png
 import numpy as np
 from skimage.measure import label
 from PIL import Image
+import torch
+from torchvision.transforms import transforms
 from tqdm.contrib.concurrent import process_map
+from tqdm import tqdm
+import network
+from functools import partial
 
 
 def get_neighbors(point, image, is_stroma):
@@ -18,20 +25,17 @@ def get_neighbors(point, image, is_stroma):
     return True
 
 
-def post_process(image_i):
-    image_path = f'r2n_test/{image_i:02d}.png'
-    image = np.asarray(Image.open(image_path))
+def post_process(index_and_image, save_dir):
+    image_i, image = index_and_image
 
     palette = [(0, 64, 128), (64, 128, 0), (243, 152, 0), (255, 255, 255)]
     with open(f'{image_i:02d}.png', 'wb') as f:
-        w = png.Writer(image.shape[1], image.shape[0],
-                       palette=palette, bitdepth=8)
+        w = png.Writer(image.shape[1], image.shape[0], palette=palette, bitdepth=8)
         w.write(f, image)
 
     image_normal = np.zeros_like(image)
     image_normal[image == 2] = 1
-    disconnect_image, disconnect_num = label(
-        image_normal, connectivity=1, return_num=True)
+    disconnect_image, disconnect_num = label(image_normal, connectivity=1, return_num=True)
 
     for i in range(1, disconnect_num+1):
         # rule 1: change small yellow blocks to its surrounding color
@@ -51,8 +55,7 @@ def post_process(image_i):
 
     image_stroma = np.zeros_like(image)
     image_stroma[image == 1] = 1
-    disconnect_image, disconnect_num = label(
-        image_stroma, connectivity=1, return_num=True)
+    disconnect_image, disconnect_num = label(image_stroma, connectivity=1, return_num=True)
 
     for i in range(1, disconnect_num+1):
         # rule 3: if one green area is circled by yellow, turn into yellow
@@ -67,11 +70,95 @@ def post_process(image_i):
             image[disconnect_image == i] = 2
 
     palette = [(0, 64, 128), (64, 128, 0), (243, 152, 0), (255, 255, 255)]
-    with open(f'r2n_test_new/{image_i:02d}_new.png', 'wb') as f:
-        w = png.Writer(image.shape[1], image.shape[0],
-                       palette=palette, bitdepth=8)
+    with open(f'{save_dir}/{image_i:02d}_new.png', 'wb') as f:
+        w = png.Writer(image.shape[1], image.shape[0], palette=palette, bitdepth=8)
         w.write(f, image)
+
+def cut_patches(im, im_size, stride, transform):
+    im_list = []
+    h, w, _ = im.shape
+
+    h_ = np.arange(0, h - im_size + 1, stride)
+    if h % stride != 0:
+        h_ = np.append(h_, h-im_size)
+    w_ = np.arange(0, w - im_size + 1, stride)
+    if w % stride != 0:
+        w_ = np.append(w_, w - im_size)
+
+    for i in h_:
+        for j in w_:   	
+            temp = Image.fromarray(np.uint8(im[i:i+im_size,j:j+im_size,:]))
+            temp = transform(temp)
+            im_list.append(temp)
+    return torch.stack(im_list)
+
+
+def predict_big_label(image_path, im_size, stride, threshold):
+    result = {}
+    # load scalenet model specially for big images
+    net = network.scalenet101(structure_path='structures/scalenet101.json')
+    path = "modelstates/big_scalenet101_last.pth"
+    pretrained = torch.load(path)['model']
+    pretrained = {k[7:] : v for k, v in pretrained.items()}
+    net.load_state_dict(pretrained)
+    net.cuda()
+    net.eval()
+    print(f'Model loaded from {path} Successfully')
+
+    # load images and cut to small patches
+    image_names = os.listdir(image_path)
+    for image in tqdm(image_names):
+        im = np.asarray(Image.open(os.path.join(image_path, image)))
+        transform = transforms.Compose([
+                    transforms.Resize((448, 448)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        im_list = cut_patches(im, im_size=im_size, stride=stride, transform=transform).cuda()
+        label = [0, 0, 0]
+        total_scores = []
+        
+        # pass each sub crops to the model, here combine 64 sub crops together as one crop to the network
+        for ims in torch.split(im_list, 4):
+            with torch.no_grad():
+                ims.cuda()
+                scores = torch.sigmoid(net(ims))
+                total_scores.append(scores.cpu().numpy())
+        scores = np.vstack(total_scores)
+
+        # for each kind of cells
+        for i in range(3):
+            # if 1 of the crop is predicted as positive, the whole picture will be positive
+            if sum(scores[: ,i] > threshold[i]) >= 1:
+                label[i] = 1
+        result[image] = label
+    
+    # store the result in prediction.json, the format is 
+    with open('prediction.json','w') as f:
+        json.dump(result, f)
+
+def load_and_scale_cam(segment_cam_path, tumor_ratio=0.9, with_big_label=False):
+    # load cam from the dir
+    npy_list = os.listdir(segment_cam_path)
+    cam_list = []
+    for cam_npy in npy_list:
+        cam = np.load(os.path.join(segment_cam_path, cam_npy))
+        if with_big_label:
+            with open('prediction.json') as json_file:
+                prediction_labels = json.load(json_file)
+            big_label = prediction_labels[f'{cam[0:2]}.png']
+            cam = cam * big_label.reshape(3,1,1)
+        # scale the score to prevent over emphasizing on tumor
+        cam[0] = cam[0] * tumor_ratio
+        # get the max score as its label
+        cam = np.argmax(cam, axis=0).astype(np.uint8)
+        cam_list.append((int(cam_npy[0:2]), cam))
+    return cam_list
 
 
 if __name__ == '__main__':
-    process_map(post_process, range(80), max_workers=6)
+    threshold = [0.5, 0.5, 0.5]
+    predict_big_label('Dataset/2.validation/img', 225, 80, threshold)
+    segment_cam_path = 'WSSS_tumor/r2n_crop_cam_npy_160_320_val/'
+    img_list = load_and_scale_cam(segment_cam_path, tumor_ratio=0.9, with_big_label=True)
+    save_dir_name = 'r2n_val_new'
+    process_map(partial(post_process, save_dir=save_dir_name), img_list, max_workers=6)
