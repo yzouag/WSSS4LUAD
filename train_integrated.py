030bd1d8456b8854605198ae49de11d1dacc226e
yzouag <3310189877@qq.com>
2021-10-09 01:48:16 +0800

    debug and refactor

--- train_integrated.py
+++ train_integrated.py
@@ -1,7 +1,12 @@
+import argparse
 import torch
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import os
+from torchvision import transforms
+import network
+import dataset
+from torch.utils.data import DataLoader
 
 def train_small_label(net, train_loader, valid_loader, optimizer, criteria, scheduler, epochs, save_every, setting_str):
     """
@@ -89,6 +94,7 @@ def train_small_label(net, train_loader, valid_loader, optimizer, criteria, sche
     plt.plot(loss_t)
     plt.ylabel('loss')
     plt.xlabel('epochs')
+    plt.title('train loss')
     plt.savefig('./image/train_loss.png')
     plt.close()
 
@@ -96,12 +102,14 @@ def train_small_label(net, train_loader, valid_loader, optimizer, criteria, sche
     plt.plot(accuracy_t)
     plt.ylabel('accuracy')
     plt.xlabel('epochs')
+    plt.title('train accuracy')
     plt.savefig('./image/train_accuracy.png')
 
     plt.figure(3)
     plt.plot(loss_v)
     plt.ylabel('loss')
     plt.xlabel('epochs')
+    plt.title('valid loss')
     plt.savefig('./image/valid_loss.png')
     plt.close()
 
@@ -109,4 +117,54 @@ def train_small_label(net, train_loader, valid_loader, optimizer, criteria, sche
     plt.plot(accuracy_v)
     plt.ylabel('accuracy')
     plt.xlabel('epochs')
+    plt.title('valid accuracy')
     plt.savefig('./image/valid_accuracy.png')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch", default=64, type=int)
+    parser.add_argument("-epoch", default=30, type=int)
+    parser.add_argument("-lr", default=0.001, type=float)
+    parser.add_argument("-resize", default=448, type=int)
+    parser.add_argument("-step", default=10, type=int)
+    parser.add_argument("-save_every", default=10, type=int, help="how often to save a model while training")
+    parser.add_argument("-gamma", default=0.1, type=float)
+    parser.add_argument('-d','--device', nargs='+', help='GPU id to use parallel', required=True, type=int)
+    parser.add_argument('-m', type=str, help='the save model name', required=True)
+    args = parser.parse_args()
+
+    batch_size = args.batch
+    epochs = args.epoch
+    base_lr = args.lr
+    resize = args.resize
+    step_size = args.step
+    save_every = args.save_every
+    gamma = args.gamma
+    devices = args.device
+    setting_str = args.m
+
+    net = network.scalenet101(structure_path='../structures/scalenet101.json', ckpt='../weights/scalenet101.pth')
+    net = torch.nn.DataParallel(net, device_ids=devices).cuda()
+    transform = transforms.Compose([
+        transforms.Resize((resize, resize)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    TrainDataset = dataset.DoubleLabelDataset(transform=transform)
+    print("train Dataset", len(TrainDataset))
+    TrainDatasampler = torch.utils.data.RandomSampler(TrainDataset)
+    TrainDataloader = DataLoader(TrainDataset, batch_size=batch_size, num_workers=2, sampler=TrainDatasampler, drop_last=True)
+    
+    ValidDataset = dataset.ValidationDataset(transform=transform)
+    print("valid Dataset", len(ValidDataset))
+    ValidDataloader = DataLoader(ValidDataset, batch_size=batch_size, num_workers=2, drop_last=True)
+
+    optimizer = torch.optim.SGD(net.parameters(), base_lr, momentum=0.9, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+    criteria = torch.nn.BCEWithLogitsLoss(reduction='mean')
+    criteria.cuda()
+
+    train_small_label(net, TrainDataloader, ValidDataloader, optimizer, criteria, scheduler, epochs, save_every, setting_str)
