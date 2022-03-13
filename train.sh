dc5e20dca38aba59ba0b6ec616df588b893b117a
yzouag <yzouag@connect.ust.hk>
2022-03-13 14:53:25 +0800

    add run script

--- train.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python main.py -d 0 -m glas_112112 -resnet -dataset warwick -test_every 4 -epoch 40
-# CUDA_VISIBLE_DEVICES=3 python prepare_seg_inputs.py -d 0 -dataset warwick -ckpt resnet_glas_11256_new_best
-# python join_crops_back.py
-# CUDA_VISIBLE_DEVICES=2 python main.py -d 0 -m glas_11256_new -resnet -dataset warwick -test -ckpt resnet_glas_11256_new_best
